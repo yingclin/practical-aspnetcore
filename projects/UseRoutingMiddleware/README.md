287b38c83426aa00d25389ce2ee3f5f860e1752a
yingclin <yingclin@hotmail.com>
2019-01-14 15:44:06 +0800

    完成 UseRoutingMiddleware

--- projects/UseRoutingMiddleware/README.md
+++ projects/UseRoutingMiddleware/README.md
@@ -1,1 +1,7 @@
-# UseRoutingMiddleware 
+# 使用路由中介軟體 - UseRoutingMiddleware 
+
+在 ConfigureServices() 中新增路由服務後，即可在 Configure() 中對路由做客製設定。
+
+AddMvc() 內部已新增路由服務。
+
+建立路由的擴充方法 - [RequestDelegateRouteBuilderExtensions](https://docs.microsoft.com/zh-tw/dotnet/api/microsoft.aspnetcore.routing.requestdelegateroutebuilderextensions?view=aspnetcore-2.1)
--- projects/UseRoutingMiddleware/Startup.cs
+++ projects/UseRoutingMiddleware/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace UseRoutingMiddleware
@@ -14,17 +15,54 @@ namespace UseRoutingMiddleware
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddRouting();
         }
 
+        /// <summary>
+        /// 路由必須設定在 Startup.Configure 方法中。 範例應用程式使用下列 API：
+        /// * RouteBuilder
+        /// * MapGet – 僅符合 HTTP GET 要求。
+        /// * UseRouter
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            // 啟用 index.html
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
-            app.UseMvc();
+            // 自定義 Route Handler
+            var trackPackageRouteHandler = new RouteHandler(context => {
+                var routeValues = context.GetRouteData().Values;
+                return context.Response.WriteAsync($"Hello! Route values: {string.Join(", ", routeValues)}");
+            });
+
+            var routeBuilder = new RouteBuilder(app, trackPackageRouteHandler);
+
+            // 建立 3 個路由規則：
+            // 1. /package，使用自定義的 Handler trackPackageRouteHandler
+            // 2. /hello，並限定 http method = GET
+            // 2. /hi，並限定 http method = POST
+
+            routeBuilder.MapRoute(
+                "Track Package Route", // name
+                "package/{operation:regex(^track|create|detonate$)}/{id:int}"); // templates
+
+            routeBuilder.MapGet("hello/{name}", context =>
+            {
+                var name = context.GetRouteValue("name");
+                return context.Response.WriteAsync($"Hello, {name}");
+            });
+
+            routeBuilder.MapPost("hi/{name}", context =>
+            {
+                var name = context.GetRouteValue("name");
+                return context.Response.WriteAsync($"Hi, {name}");
+            });
+
+            var routes = routeBuilder.Build();
+            app.UseRouter(routes);
         }
     }
 }
