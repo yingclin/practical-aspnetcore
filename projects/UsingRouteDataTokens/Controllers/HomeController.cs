da78043b6bae07e12cbe58ae7f1fcae035748d3d
yingclin <yingclin@hotmail.com>
2019-01-15 14:11:29 +0800

    new UsingRouteDataTokens

---
+++ projects/UsingRouteDataTokens/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsingRouteDataTokens.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+          //TODO: Implement Realistic Implementation
+          return View();
+        }
+    }
+}
---
+++ projects/UsingRouteDataTokens/README.md
@@ -0,0 +1,1 @@
+# UsingRouteDataTokens 
