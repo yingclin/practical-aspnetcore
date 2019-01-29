57fac69d1e521c82cfc910089f7e2cb9b4396cfb
yingclin <yingclin@hotmail.com>
2019-01-29 16:20:29 +0800

    完成 project : ConfigurationConvertToClass

--- projects/ConfigurationConvertToClass/Pages/Index.cshtml.cs
+++ projects/ConfigurationConvertToClass/Pages/Index.cshtml.cs
@@ -4,13 +4,61 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace ConfigurationConvertToClass.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public void OnGet()
         {
         }
+
+        public string[] GetWidgetsKeys() => _configuration.AsEnumerable()
+            .Select(o => o.Key)
+            .Where(o => o.Contains("widgets"))
+            .ToArray();
+
+        // 以 Get 方式轉設定值
+        public Widgets GetWidgets() => _configuration.GetSection("widgets").Get<Widgets>();
+
+        // 以 Bind 方式轉設定值
+        public Widgets BindWidgets()
+        {
+            var widgets = new Widgets();
+            _configuration.GetSection("widgets").Bind(widgets);
+            return widgets;
+        }
     }
+
+    #region Configuration classes
+
+    public class Widgets
+    {
+        public Layout Layout { get; set; }
+        public Display Display { get; set; }
+
+        public string ToJson() => JsonConvert.SerializeObject(this);
+    }
+
+    public class Layout
+    {
+        public string Name { get; set; }
+        public int Layer { get; set; }
+    }
+
+    public class Display
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+
+    #endregion
 }
--- projects/MemoryConfigurationProvider/Program.cs
+++ projects/MemoryConfigurationProvider/Program.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
 
 namespace MemoryConfigurationProvider
 {
